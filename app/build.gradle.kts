c37f4d21c5b1347a370ae80383728211681ce319
janvaliser <jan.valiser@gmail.com>
2024-12-29 18:34:24 +0100

    Enable view binding

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,10 @@ android {
     namespace = "com.example.projektapp"
     compileSdk = 35
 
+    buildFeatures {
+        viewBinding = true
+    }
+
     defaultConfig {
         applicationId = "com.example.projektapp"
         minSdk = 24
