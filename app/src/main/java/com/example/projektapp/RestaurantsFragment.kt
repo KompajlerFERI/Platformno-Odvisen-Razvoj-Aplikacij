0e88bdc800246feffec9918e0087fa45227234dc
janvaliser <jan.valiser@gmail.com>
2025-01-21 21:51:14 +0100

    [DD-367] Add restaurant searchbar

--- app/src/main/java/com/example/projektapp/RestaurantsFragment.kt
+++ app/src/main/java/com/example/projektapp/RestaurantsFragment.kt
@@ -1,5 +1,6 @@
 package com.example.projektapp
 
+import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
 import android.util.TypedValue
 import androidx.fragment.app.Fragment
@@ -7,6 +8,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.view.ViewCompat
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.lib.Restaurant
@@ -38,6 +40,14 @@ class RestaurantsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+
+        val shape = GradientDrawable().apply {
+            shape = GradientDrawable.RECTANGLE
+            cornerRadius = resources.getDimension(R.dimen.card_corner_radius)
+            setColor(resources.getColor(R.color.white, null))
+        }
+        binding.searchBar.background = shape
 
         application.restaurants = listOf()
         binding.recyclerView.layoutManager = LinearLayoutManager(requireContext())
